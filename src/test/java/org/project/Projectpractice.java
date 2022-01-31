4d4a79bd26ca538642a4b8d6f2b62efe7c0740a7
ruthrakumar07 <ruthrakumar04@gmail.com>
2022-01-31 17:07:21 +0530

    kumar's updated code

--- src/test/java/org/project/Projectpractice.java
+++ src/test/java/org/project/Projectpractice.java
@@ -3,6 +3,8 @@ package org.project;
 public class Projectpractice {
 public static void main(String[] args) {
 	System.out.println("test1");	
+	System.out.println("test4");
+	System.out.println("test5");
 }
 
 }
